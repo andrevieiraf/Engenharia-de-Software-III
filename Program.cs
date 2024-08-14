70ad7430e15b1226abdf71a318ca02185a88013d
Andre Vieira de Figueiredo <andrevieiradefigueiredo@gmail.com>
2024-08-13 21:25:46 -0300

    Nova Classe

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Classe
+{
+    class MembrosEstaticos
+    {
+        //static void Main(string[] args)
+        //{
+        //Problema Exemplo
+        //Console.Write("Entre o valor do raio: ");
+        //double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        //double circunferencia = Calculadora.Circunferencia(raio);
+        //double volume = Calculadora.Volume(raio);
+
+        //Console.WriteLine("Circunferência: " + circunferencia.ToString("F2", CultureInfo.InvariantCulture));
+        //Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+        //Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        //Exercício de Fixação
+        //Console.Write("Qual é a cotação do dólar? ");
+        //double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        //Console.Write("Quantos dólares você vai comprar? ");
+        //double dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        //double valorIOF = ConversorDeMoedas.ValorIOF(cotacao, dolares);
+        //double valorsemIOF = ConversorDeMoedas.ValorsemIOF(cotacao, dolares);
+        //double valorFinal = ConversorDeMoedas.ValorFinal(cotacao, dolares);
+
+        //Console.Write("Valor a ser pago em reais = " + valorFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+        //mais eficiente
+        //Console.Write("Qual é a cotação do dólar? ");
+        //double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        //Console.Write("Quantos dólares você vai comprar? ");
+        //double dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        //double result = ConversorDeMoedas.ValorFinal(cotacao, dolares);
+
+        //Console.Write("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        //}
+    }
+}
+
